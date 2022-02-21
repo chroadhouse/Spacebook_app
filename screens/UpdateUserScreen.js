3d1bda12f0e96d630c7753c1e2c62503d5d0c023
Charlie Roadhouse <ch.roadhouse@gmail.com>
2022-02-21 14:18:26 +0000

    Updates implemented
    A user can now update all of their information which will be stored on the server

--- screens/UpdateUserScreen.js
+++ screens/UpdateUserScreen.js
@@ -7,10 +7,14 @@ class UpdateUserScreen extends Component{
         super(props);
 
         this.state = {
-            email: "",
+            email_og: "",
             password: "",
+            first_name_og: "",
+            last_name_og: "",
+            email: "",
             first_name: "",
-            last_name: ""
+            last_name: "",
+            password_verify: ""
         }
     }
 
@@ -49,9 +53,9 @@ class UpdateUserScreen extends Component{
           })
           .then((responseJson) => {
               this.setState({
-                first_name: responseJson.first_name,
-                last_name: responseJson.last_name,
-                email: responseJson.email,
+                first_name_og: responseJson.first_name,
+                last_name_og: responseJson.last_name,
+                email_og: responseJson.email,
               })
           })
           .catch((error) => {
@@ -61,9 +65,58 @@ class UpdateUserScreen extends Component{
       }
 
       updateUser = async() =>{
-        //update user needs the ID of the user
+        //Update user needs the ID of the user
         //Then we just update all the data in there
-      }
+        //
+
+        let to_send = {};
+        
+        if(this.state.first_name != this.state.first_name_og && this.state.first_name != ""){
+            to_send['first_name'] = this.state.first_name;
+        }
+
+        if(this.state.last_name != this.state.last_name_og && this.state.last_name != ""){
+            to_send['last_name'] = this.state.last_name;
+        }
+
+        if(this.state.email != this.state.email_og && this.state.email != ""){
+            to_send['email'] = this.state.email;
+        }
+
+        if(this.state.password != "" && this.state.password === this.state.password_verify){
+            to_send['password'] = this.state.password;
+        }
+
+        console.log(JSON.stringify(to_send));
+        const token = await AsyncStorage.getItem('@session_token');
+        const id = await AsyncStorage.getItem('user_id');
+
+        return fetch("http://localhost:3333/api/1.0.0/user/" + id,{
+            'method': 'PATCH',
+            'headers': {
+              'X-Authorization': token,
+              'Content-Type': 'application/json'
+    
+            },
+            body: JSON.stringify(to_send)
+          })
+          .then((response) => {
+              if(response.status ===200){
+                this.props.navigation.navigate('profileScreen')
+              }else if(response.status === 401){
+                this.props.navigation.navigate("Login");
+              }else{
+                throw 'Something went wrong';
+              }
+          })
+          .catch((error) => {
+              console.log("Something is going wrog")
+              console.log(error);
+          })
+    }
+    
+
+      
     
       checkLoggedIn = async () => {
         const value = await AsyncStorage.getItem('@session_token');
@@ -77,25 +130,31 @@ class UpdateUserScreen extends Component{
             <View>
                 <Text>First Name</Text>
                 <TextInput
-                    placeholder={this.state.first_name} 
-                    defaultValue={this.state.first_name}
+                    placeholder={this.state.first_name_og} 
+                    //defaultValue={this.state.first_name_og}
+                    onChangeText={(first_name) => this.setState({first_name})}
+                    value={this.state.first_name}
                 />
                 <Text>Last Name</Text>
                 <TextInput
-                    placeholder={this.state.last_name}
-                    defaultValue={this.state.last_name}
+                    placeholder={this.state.last_name_og}
+                    //defaultValue={this.state.last_name_og}
                 />
                 <Text>Email</Text>
                 <TextInput
-                    placeholder={this.state.email}
-                    defaultValue={this.state.email}
+                    placeholder={this.state.email_og}
+                    //defaultValue={this.state.email_og}
                 />
-                <Text>Must enter password for information to be saved</Text>
-                <Text>Password</Text>
+
+                <Text>Update a new Password</Text>
                 <TextInput
                     placeholder="Password here"
                 />
                 <Text>Verify Password</Text>
+                <Button
+                    title="Save Data"
+                    onPress={() => this.updateUser()}
+                />
             </View>
           );
       }
